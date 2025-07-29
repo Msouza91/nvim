b1269492373aff2f82eecb49f7bb5df2e396877e
Marcos <marcos.souza@contractor.rhimagnesita.com>
2025-07-29 14:30:05 -0300

    obsidian keymaps

--- lua/marcos/plugins/obsidian.lua
+++ lua/marcos/plugins/obsidian.lua
@@ -13,79 +13,134 @@ return {
 			.. "/vaults/Deez-notes/*.md",
 		"BufNewFile " .. vim.fn.expand("~") .. "/vaults/Deez-notes/*.md",
 	},
-	---@module 'obsidian'
-	---@type obsidian.config
-	opts = {
-		legacy_commands = false,
-		workspaces = {
+	config = function()
+		local opts = {
+			legacy_commands = false,
+			workspaces = {
+				{
+					name = "personal",
+					path = "~/vaults/Deez-notes",
+				},
+				-- {
+				-- 	name = "work",
+				-- 	path = "~/vaults/work",
+				-- },
+			},
+			daily_notes = {
+				-- Optional, if you keep daily notes in a separate directory.
+				folder = "Daily-notes",
+				-- Optional, if you want to change the date format for the ID of daily notes.
+				date_format = "%d-%m-%y",
+				-- Optional, if you want to change the date format of the default alias of daily notes.
+				alias_format = "%B %-d, %Y",
+				-- Optional, default tags to add to each new daily note created.
+				default_tags = { "daily", "daily-notes" },
+				-- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
+				template = "daily.md",
+				-- Optional, if you want `Obsidian yesterday` to return the last work day or `Obsidian tomorrow` to return the next work day.
+				workdays_only = true,
+			},
+			preferred_link_style = "markdown",
+			completion = {
+				-- Enables completion using nvim_cmp
+				nvim_cmp = false,
+				-- Enables completion using blink.cmp
+				blink = true,
+				-- Trigger completion at 2 chars.
+				min_chars = 2,
+				-- Set to false to disable new note creation in the picker
+				create_new = true,
+			},
+
+			picker = {
+				-- Set your preferred picker. Can be one of 'telescope.nvim', 'fzf-lua', 'mini.pick' or 'snacks.pick'.
+				name = "snacks.pick",
+				-- Optional, configure key mappings for the picker. These are the defaults.
+				-- Not all pickers support all mappings.
+				note_mappings = {
+					-- Create a new note from your query.
+					new = "<C-x>",
+					-- Insert a link to the selected note.
+					insert_link = "<C-l>",
+				},
+				tag_mappings = {
+					-- Add tag(s) to current note.
+					tag_note = "<C-x>",
+					-- Insert a tag at the current location.
+					insert_tag = "<C-l>",
+				},
+			},
+
+			templates = {
+				folder = "Templates",
+				date_format = "%Y-%m-%d",
+				time_format = "%H:%M",
+				-- A map for custom variables, the key should be the variable and the value a function.
+				-- Functions are called with obsidian.TemplateContext objects as their sole parameter.
+				-- See: https://github.com/obsidian-nvim/obsidian.nvim/wiki/Template#substitutions
+				substitutions = {},
+
+				-- A map for configuring unique directories and paths for specific templates
+				--- See: https://github.com/obsidian-nvim/obsidian.nvim/wiki/Template#customizations
+				customizations = {},
+			},
+			ui = { enable = false },
+		}
+
+		require("obsidian").setup(opts)
+
+		-- Key mappings
+		local mappings = {
 			{
-				name = "personal",
-				path = "~/vaults/Deez-notes",
+				"<leader>os",
+				"<cmd>Obsidian search<cr>",
+				{ desc = "Obsidian Search" },
 			},
-			-- {
-			-- 	name = "work",
-			-- 	path = "~/vaults/work",
-			-- },
-		},
-		daily_notes = {
-			-- Optional, if you keep daily notes in a separate directory.
-			folder = "Daily-notes",
-			-- Optional, if you want to change the date format for the ID of daily notes.
-			date_format = "%d-%m-%y",
-			-- Optional, if you want to change the date format of the default alias of daily notes.
-			alias_format = "%B %-d, %Y",
-			-- Optional, default tags to add to each new daily note created.
-			default_tags = { "daily", "daily-notes" },
-			-- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
-			template = "daily.md",
-			-- Optional, if you want `Obsidian yesterday` to return the last work day or `Obsidian tomorrow` to return the next work day.
-			workdays_only = true,
-		},
-		preferred_link_style = "markdown",
-		completion = {
-			-- Enables completion using nvim_cmp
-			nvim_cmp = false,
-			-- Enables completion using blink.cmp
-			blink = true,
-			-- Trigger completion at 2 chars.
-			min_chars = 2,
-			-- Set to false to disable new note creation in the picker
-			create_new = true,
-		},
-
-		picker = {
-			-- Set your preferred picker. Can be one of 'telescope.nvim', 'fzf-lua', 'mini.pick' or 'snacks.pick'.
-			name = "snacks.pick",
-			-- Optional, configure key mappings for the picker. These are the defaults.
-			-- Not all pickers support all mappings.
-			note_mappings = {
-				-- Create a new note from your query.
-				new = "<C-x>",
-				-- Insert a link to the selected note.
-				insert_link = "<C-l>",
+			{
+				"<leader>ost",
+				"<cmd>Obsidian tags<cr>",
+				{ desc = "Obsidian Search Tag Instances" },
 			},
-			tag_mappings = {
-				-- Add tag(s) to current note.
-				tag_note = "<C-x>",
-				-- Insert a tag at the current location.
-				insert_tag = "<C-l>",
+			{
+				"<leader>oit",
+				"<cmd>Obsidian template<cr>",
+				{ desc = "Obsidian insert template" },
 			},
-		},
+			{
+				"<leader>ot",
+				"<cmd>Obsidian today<cr>",
+				{ desc = "Obsidian Today Daily Note" },
+			},
+			{
+				"<leader>oT",
+				"<cmd>Obsidian tomorrow<cr>",
+				{ desc = "Obsidian Tomorrow Daily Note" },
+			},
+			{
+				"<leader>oy",
+				"<cmd>Obsidian yesterday<cr>",
+				{ desc = "Obsidian Yesterday Daily Note" },
+			},
+			{
+				"<leader>on",
+				"<cmd>Obsidian new<cr>",
+				{ desc = "Obsidian New Note" },
+			},
+			{
+				"<leader>oN",
+				"<cmd>Obsidian new_from_template<cr>",
+				{ desc = "Obsidian New Note From Template" },
+			},
+			{
+				"<leader>ob",
+				"<cmd>Obsidian backlinks<cr>",
+				{ desc = "Obsidian Backlinks on Picker" },
+			},
+		}
 
-		templates = {
-			folder = "Templates",
-			date_format = "%Y-%m-%d",
-			time_format = "%H:%M",
-			-- A map for custom variables, the key should be the variable and the value a function.
-			-- Functions are called with obsidian.TemplateContext objects as their sole parameter.
-			-- See: https://github.com/obsidian-nvim/obsidian.nvim/wiki/Template#substitutions
-			substitutions = {},
-
-			-- A map for configuring unique directories and paths for specific templates
-			--- See: https://github.com/obsidian-nvim/obsidian.nvim/wiki/Template#customizations
-			customizations = {},
-		},
-
-		ui = { enable = false },
-	},
+		for _, map in ipairs(mappings) do
+			local mode = map.mode or "n"
+			vim.keymap.set(mode, map[1], map[2], { desc = map[3].desc, noremap = true, silent = true })
+		end
+	end,
 }
