688e9fb9863bd8d74ad01b2db99b3d0e05298a49
Marcos <marcos.souza@contractor.rhimagnesita.com>
2025-07-30 12:27:49 -0300

    fancier home dash

--- lua/marcos/extra/snacks-dash.lua
+++ lua/marcos/extra/snacks-dash.lua
@@ -49,7 +49,8 @@ function M.opts()
 		formats = {
 			icon = function(item)
 				if item.file and (item.icon == "file" or item.icon == "directory") then
-					return M.icon(item.file, item.icon)
+					local icon = item.icon == "directory" and " " or " " -- Slightly modified from the default config since it was moved here where I didn't run the Snacks' setup func yet
+					return { icon, width = 2, hl = "icon" }
 				end
 				return { item.icon, width = 2, hl = "icon" }
 			end,
@@ -72,7 +73,30 @@ function M.opts()
 		},
 		sections = {
 			{ section = "header" },
+			{
+				pane = 2,
+				section = "terminal",
+				cmd = "colorscript -e square",
+				height = 5,
+				padding = 1,
+			},
 			{ section = "keys", gap = 1, padding = 1 },
+			{ pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+			{ pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+			{
+				pane = 2,
+				icon = " ",
+				title = "Git Status",
+				section = "terminal",
+				enabled = function()
+					return Snacks.git.get_root() ~= nil
+				end,
+				cmd = "git status --short --branch --renames",
+				height = 5,
+				padding = 1,
+				ttl = 5 * 60,
+				indent = 3,
+			},
 			{ section = "startup" },
 		},
 	}
