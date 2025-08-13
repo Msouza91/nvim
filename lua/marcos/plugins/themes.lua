a7c38549a481d86f398c7c368afd02282a16cf42
Marcos Souza <me@msouza.tech>
2025-08-13 07:26:43 -0300

    friendship ended with gruvbox, terafox is my new bf

--- lua/marcos/plugins/themes.lua
+++ lua/marcos/plugins/themes.lua
@@ -1,33 +1,16 @@
 return {
-	"ellisonleao/gruvbox.nvim",
-	name = "gruvbox",
+	"EdenEast/nightfox.nvim",
+	name = "nightfox",
 	lazy = false,
 	config = function()
 		local opts = {
-			terminal_colors = true, -- add neovim terminal colors
-			undercurl = true,
-			underline = true,
-			bold = true,
-			italic = {
-				strings = true,
-				emphasis = true,
-				comments = true,
-				operators = false,
-				folds = true,
+			styles = {
+				comments = "italic",
+				functions = "italic,bold",
 			},
-			strikethrough = true,
-			invert_selection = false,
-			invert_signs = false,
-			invert_tabline = false,
-			inverse = true, -- invert background for search, diffs, statuslines and errors
-			contrast = "hard", -- can be "hard", "soft" or empty string
-			palette_overrides = {},
-			overrides = {},
-			dim_inactive = false,
-			transparent_mode = false,
 		}
-		require("gruvbox").setup(opts)
-		vim.cmd("colorscheme gruvbox")
+		require("nightfox").setup(opts)
+		vim.cmd("colorscheme terafox")
 		--vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 		--vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 	end,
